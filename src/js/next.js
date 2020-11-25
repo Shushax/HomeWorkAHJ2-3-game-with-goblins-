7f746b4db15425b990eee50e3b8c011840776714
Ksenia Sidorova <nekfi@mail.ru>
2020-11-25 06:28:09 +0300

    Правки, еслинт

--- src/js/next.js
+++ src/js/next.js
@@ -1,7 +1,8 @@
+/* eslint import/no-cycle:0 */ // --> OFF
 import start from './start';
 
 export default function next() {
-    const lastImg = document.getElementsByTagName('img')[0];
-    lastImg.remove();
-    start();
-  }
+  const lastImg = document.getElementsByTagName('img')[0];
+  lastImg.remove();
+  start();
+}
--- src/js/start.js
+++ src/js/start.js
@@ -1,26 +1,29 @@
+/* eslint import/no-cycle:0 */ // --> OFF
+/* eslint no-const-assign:0 */ // --> OFF
+
 import goblin from '../img/goblin.png';
-import {intervalID} from './app'
+import { intervalID } from './app';
 
 export const text = document.getElementsByTagName('h1')[0];
-export let number = 0;
+let number = 0;
 
 export default function start() {
-    const img = document.createElement('img');
-    img.src = goblin;
-  
-    const id = Math.floor(Math.random() * 16);
-    const td = document.getElementById(`${id}`);
-    td.appendChild(img);
-    text.textContent = `Ты пропустил гоблинов: ${number}`;
-    number++;
-    if (number > 5) {
-      const loose = document.createElement('h2');
-      document.body.appendChild(loose);
-      loose.textContent = 'Ты проиграл! Игра закончена :(';
-      clearInterval(intervalID);
-      td.innerHTML = '';
-    }
-    td.onclick = function() {
-      number--;
-    }
+  const img = document.createElement('img');
+  img.src = goblin;
+
+  const id = Math.floor(Math.random() * 16);
+  const td = document.getElementById(`${id}`);
+  td.appendChild(img);
+  text.textContent = `Ты пропустил гоблинов: ${number}`;
+  number += number;
+  if (number > 5) {
+    const loose = document.createElement('h2');
+    document.body.appendChild(loose);
+    loose.textContent = 'Ты проиграл! Игра закончена :(';
+    clearInterval(intervalID);
+    td.innerHTML = '';
   }
+  td.onclick = function () {
+    number -= number;
+  };
+}
